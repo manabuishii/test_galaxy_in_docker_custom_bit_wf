481705f8470c87c12dcef838dbb1b5a34ca284d0
Manabu ISHII <manabu.ishii.rb@gmail.com>
2016-02-04 17:22:21 +0900

    check ADMIN phrase

--- test_galaxy.rb
+++ test_galaxy.rb
@@ -1,65 +1,93 @@
 require 'selenium-webdriver'
+require 'rubygems'
+require 'test/unit'
 
-username="admin@galaxy.org"
-password="admin"
+$url = ARGV[0]
 
-client = Selenium::WebDriver::Remote::Http::Default.new
-client.timeout = 120
-puts "Connect to #{ARGV[0]}"
+class GalaxyTest < Test::Unit::TestCase
+  def setup
+    # create selenium objects
+    #@driver = Selenium::WebDriver.for :firefox
+    #@wait = Selenium::WebDriver::Wait.new :timeout => 10
+  end
+  def test_galaxy
 
-driver = Selenium::WebDriver.for :firefox, :http_client => client
-#
-count=0
-driver.get(ARGV[0])
-puts "webdriver title on google.com : #{driver.title}"
-driver.save_screenshot("/work/galaxy-#{count}.png")
+    username="admin@galaxy.org"
+    password="admin"
 
-#
-element = driver.find_element(:id, 'user')
-user=element.find_elements(:xpath, "//a[contains(@title, 'Account registration or login')]")
-user[0].click
+    client = Selenium::WebDriver::Remote::Http::Default.new
+    client.timeout = 120
+    puts "Connect to #{$url}"
 
-count=count+1
-driver.save_screenshot("/work/galaxy-#{count}.png")
-#
-userlogin=element.find_elements(:xpath, "//a[contains(@href, '/user/login')]")
-userlogin[0].click
+    driver = Selenium::WebDriver.for :firefox, :http_client => client
+    #
+    count=0
+    driver.get($url)
+    puts "webdriver title on galaxy : #{driver.title}"
+    driver.save_screenshot("/work/galaxy-#{count}.png")
+
+    # Check you are not admin
+    begin
+      admin_field=driver.find_element(:id, 'admin')
+      assert( false, "fail you are already admin")
+    rescue
+      # you are not admin
+    end
+
+    p driver.page_source
+    element = driver.find_element(:id, 'user')
+    user=element.find_elements(:xpath, "//a[contains(@title, 'Account registration or login')]")
+    user[0].click
+
+    count=count+1
+    driver.save_screenshot("/work/galaxy-#{count}.png")
+    #
+    userlogin=element.find_elements(:xpath, "//a[contains(@href, '/user/login')]")
+    userlogin[0].click
 
 
-count=count+1
-driver.save_screenshot("/work/galaxy-#{count}.png")
-#
-driver.switch_to.frame('galaxy_main')
-loginform=driver.find_element(:id, 'login')
-inputuser=loginform.find_elements(:xpath, "//input[contains(@name, 'login')]")[0]
-inputuser.send_keys(username)
-passworduser=loginform.find_elements(:xpath, "//input[contains(@name, 'password')]")[0]
-passworduser.send_keys(password)
+    count=count+1
+    driver.save_screenshot("/work/galaxy-#{count}.png")
+    # Input Login and Password
+    driver.switch_to.frame('galaxy_main')
+    loginform=driver.find_element(:id, 'login')
+    inputuser=loginform.find_elements(:xpath, "//input[contains(@name, 'login')]")[0]
+    inputuser.send_keys(username)
+    passworduser=loginform.find_elements(:xpath, "//input[contains(@name, 'password')]")[0]
+    passworduser.send_keys(password)
 
-count=count+1
-driver.save_screenshot("/work/galaxy-#{count}.png")
+    count=count+1
+    driver.save_screenshot("/work/galaxy-#{count}.png")
 
-#
-loginbutton=loginform.find_elements(:xpath, "//input[contains(@name, 'login_button')]")[0]
-loginbutton.click
-sleep(5)
+    # Click Login button
+    loginbutton=loginform.find_elements(:xpath, "//input[contains(@name, 'login_button')]")[0]
+    loginbutton.click
+    sleep(5)
 
-count=count+1
-driver.save_screenshot("/work/galaxy-#{count}.png")
-#
-element = driver.find_element(:id, 'user')
-user=element.find_elements(:xpath, "//a[contains(@title, 'Account preferences and saved data')]")[0]
-user.click
-sleep(1)
+    count=count+1
+    driver.save_screenshot("/work/galaxy-#{count}.png")
+    # if login is fail, next line has error
+    element = driver.find_element(:id, 'user')
+    user=element.find_elements(:xpath, "//a[contains(@title, 'Account preferences and saved data')]")[0]
+    user.click
+    sleep(1)
 
-count=count+1
-driver.save_screenshot("/work/galaxy-#{count}.png")
-#
-parent=user.find_element(:xpath, "./../ul/li[1]/a")
+    count=count+1
+    driver.save_screenshot("/work/galaxy-#{count}.png")
+    #
+    parent=user.find_element(:xpath, "./../ul/li[1]/a")
 
-count=count+1
-driver.save_screenshot("/work/galaxy-#{count}.png")
-#
-p parent.text
-#
-driver.close
+    count=count+1
+    driver.save_screenshot("/work/galaxy-#{count}.png")
+    #
+    p parent.text
+    #
+    begin
+      admin_field=driver.find_element(:id, 'admin')
+    rescue
+      assert false, "ADMIN required"
+    end
+    #
+    driver.close
+  end
+end
