349b72d2e37c5da9486bc710cb25b185c7acd98a
Manabu ISHII <manabu.ishii.rb@gmail.com>
2016-02-25 20:51:52 +0900

    Add TODO

--- run-container.sh
+++ run-container.sh
@@ -6,10 +6,12 @@ SCRIPT=$2
 GALAXYHOST=$3
 
 mkdir -p images
+# TODO change rm -rf to something
 docker-machine ssh ${DOCKERHOST} "sudo rm -rf $PWD ; sudo mkdir -p $PWD; sudo chmod 777 $PWD"
 for RUBYFILE in $(ls *.rb)
 do
   docker-machine scp ${RUBYFILE} ${DOCKERHOST}:$PWD
 done
 docker $(docker-machine config ${DOCKERHOST} ) run --rm -v $PWD:/work ebeltran/webdriver-ruby ruby /work/${SCRIPT} ${GALAXYHOST}
+# TODO timestamp to file
 docker-machine scp -r ${DOCKERHOST}:$PWD/*.png images/
