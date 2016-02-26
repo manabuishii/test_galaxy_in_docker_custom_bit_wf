8e2a60620668ebdc5bd0d3a442ebc053c511a652
Manabu ISHII <manabu.ishii.rb@gmail.com>
2016-02-26 15:47:45 +0900

    multiselect _1 and _2

--- test_galaxy_RNA-seq_01_pair_end_express.rb
+++ test_galaxy_RNA-seq_01_pair_end_express.rb
@@ -321,46 +321,46 @@ class GalaxyTest < Test::Unit::TestCase
     sleep 1
     count=count+1
     driver.save_screenshot("/work/galaxy-#{count}.png")
-    # # Step 2 multiselect _1
-    # # Input Dataset [Single end reads]
-    # element = driver.find_elements(:xpath, "//label[contains(.,'Input Dataset [Left-hand of Paired End reads]')]")[0]
-    # element2 = element.find_elements(:xpath, "./span[contains(@class,'multiinput')]")[0]
-    # element2.click
-    # # element = driver.find_elements(:xpath, "//label[contains(.,'Input Dataset [Single end reads]')]/span[contains(@class,'multiinput')]")[0]
-    # # p element.attribute('innerHTML')
-    # # element.click
-    # sleep 1
-    # count=count+1
-    # driver.save_screenshot("/work/galaxy-#{count}.png")
-    # multiinput_filter = element.find_elements(:xpath, "..//input[@class='multiinput-filter']")[0]
-    # multiinput_filter.click
-    # count=count+1
-    # driver.save_screenshot("/work/galaxy-#{count}.png")
-    # # input _1 and enter to select
-    # multiinput_filter.send_keys("_1\n")
-    # sleep 1
-    # count=count+1
-    # driver.save_screenshot("/work/galaxy-#{count}.png")
-    # # Step 3 multiselect _2
-    # # Input Dataset [Right-hand mates for Paired End Reads]
-    # element = driver.find_elements(:xpath, "//label[contains(.,'Input Dataset [Right-hand mates for Paired End Reads]')]")[0]
-    # element2 = element.find_elements(:xpath, "./span[contains(@class,'multiinput')]")[0]
-    # element2.click
-    # # element = driver.find_elements(:xpath, "//label[contains(.,'Input Dataset [Single end reads]')]/span[contains(@class,'multiinput')]")[0]
-    # # p element.attribute('innerHTML')
-    # # element.click
-    # sleep 1
-    # count=count+1
-    # driver.save_screenshot("/work/galaxy-#{count}.png")
-    # multiinput_filter = element.find_elements(:xpath, "..//input[@class='multiinput-filter']")[0]
-    # multiinput_filter.click
-    # count=count+1
-    # driver.save_screenshot("/work/galaxy-#{count}.png")
-    # # input _1 and enter to select
-    # multiinput_filter.send_keys("_2\n")
-    # sleep 1
-    # count=count+1
-    # driver.save_screenshot("/work/galaxy-#{count}.png")
+    # Step 2 multiselect _1
+    # Input Dataset [Single end reads]
+    element = driver.find_elements(:xpath, "//label[contains(.,'Input Dataset [Left-hand of Paired End reads]')]")[0]
+    element2 = element.find_elements(:xpath, "./span[contains(@class,'multiinput')]")[0]
+    element2.click
+    # element = driver.find_elements(:xpath, "//label[contains(.,'Input Dataset [Single end reads]')]/span[contains(@class,'multiinput')]")[0]
+    # p element.attribute('innerHTML')
+    # element.click
+    sleep 1
+    count=count+1
+    driver.save_screenshot("/work/galaxy-#{count}.png")
+    multiinput_filter = element.find_elements(:xpath, "..//input[@class='multiinput-filter']")[0]
+    multiinput_filter.click
+    count=count+1
+    driver.save_screenshot("/work/galaxy-#{count}.png")
+    # input _1 and enter to select
+    multiinput_filter.send_keys("_1\n")
+    sleep 1
+    count=count+1
+    driver.save_screenshot("/work/galaxy-#{count}.png")
+    # Step 3 multiselect _2
+    # Input Dataset [Right-hand mates for Paired End Reads]
+    element = driver.find_elements(:xpath, "//label[contains(.,'Input Dataset [Right-hand mates for Paired End Reads]')]")[0]
+    element2 = element.find_elements(:xpath, "./span[contains(@class,'multiinput')]")[0]
+    element2.click
+    # element = driver.find_elements(:xpath, "//label[contains(.,'Input Dataset [Single end reads]')]/span[contains(@class,'multiinput')]")[0]
+    # p element.attribute('innerHTML')
+    # element.click
+    sleep 1
+    count=count+1
+    driver.save_screenshot("/work/galaxy-#{count}.png")
+    multiinput_filter = element.find_elements(:xpath, "..//input[@class='multiinput-filter']")[0]
+    multiinput_filter.click
+    count=count+1
+    driver.save_screenshot("/work/galaxy-#{count}.png")
+    # input _1 and enter to select
+    multiinput_filter.send_keys("_2\n")
+    sleep 1
+    count=count+1
+    driver.save_screenshot("/work/galaxy-#{count}.png")
     #
     # # Step 7: Sailfish_Wrapper
     # element = driver.find_elements(:xpath, "//span[contains(.,'Step 7: Sailfish_Wrapper')]")[0]
